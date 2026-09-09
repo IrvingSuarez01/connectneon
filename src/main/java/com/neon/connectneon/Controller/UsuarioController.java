b71db52de33129e611d8a919146804ccb4113042
IrvingSuarez <irving.suarez01@outlook.com>
2026-09-09 15:31:21 -0600

    Se agregan endpoints pendientes se corrigue el nombre de la entidad de la tabla de usuario

--- src/main/java/com/neon/connectneon/Controller/UsuarioController.java
+++ src/main/java/com/neon/connectneon/Controller/UsuarioController.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,4 +40,36 @@ public class UsuarioController {
        // 200ok
        return ResponseEntity.ok(usuarios);
    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioResponse> buscarPorID(@PathVariable Long id){
+
+       UsuarioResponse usuario= usurioservice.buscarPorID(id);
+
+       if(usuario == null){
+           return  ResponseEntity.notFound().build();
+       }
+       return ResponseEntity.ok(usuario);
+   }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioResponse> actuzalizar(@PathVariable Long id,@RequestBody UsuarioRequest request)
+    {
+
+        UsuarioResponse usuarioActulziado = usurioservice.actualizar(id,request);
+        if(usuarioActulziado == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(usuarioActulziado);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminar(@PathVariable Long id){
+        boolean eliminado = usurioservice.eliminar(id);
+        if(!eliminado){
+            return  ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.noContent().build();
+    }
+
 }
